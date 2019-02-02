0f8f7cf3f7727e8fc455d9319077d64892479249
Timothy Andrew <mail@timothyandrew.net>
2019-02-02 12:39:34 +0530

    fetch image list

--- node_helper.js
+++ node_helper.js
@@ -14,6 +14,7 @@
 
 // call in the required classes
 var NodeHelper = require('node_helper');
+var http = require('http');
 var FileSystemImageSlideshow = require('fs');
 
 // the main module helper create
@@ -22,84 +23,53 @@ module.exports = NodeHelper.create({
   start: function() {
     //this.moduleConfigs = [];
   },
-  // shuffles an array at random and returns it
-  shuffleArray: function(array) {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  },
-  // sort by filename attribute
-  sortByFilename: function(a, b) {
-    aL = a.toLowerCase();
-    bL = b.toLowerCase();
-    if (aL > bL) return 1;
-    else return -1;
-  },
-  // checks there's a valid image file extension
-  checkValidImageFileExtension: function(filename, extensions) {
-    var extList = extensions.split(',');
-    for (var extIndex = 0; extIndex < extList.length; extIndex++) {
-      if (filename.toLowerCase().endsWith(extList[extIndex])) return true;
-    }
-    return false;
-  },
   // gathers the image list
-  gatherImageList: function(config) {
-    var self = this;
-    // create an empty main image list
-    var imageList = [];
-    for (var i = 0; i < config.imagePaths.length; i++) {
-      this.getFiles(config.imagePaths[i], imageList, config);
-    }
+  gatherImageList: function(config, callback) {
+    var options = {
+      hostname: 'random-photo-path',
+      port: 9501,
+      path: '/api/data.json',
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
+    };
+    
+    var req = http.request(options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (data) {
+        var imageList = JSON.parse(data);
+        callback(null, data.imageList);
+      });
+    });
 
-    imageList = config.randomizeImageOrder
-      ? this.shuffleArray(imageList)
-      : imageList.sort(this.sortByFilename);
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+      callback(e);
+    });
 
-    return imageList;
+    req.end();
   },
 
-  getFiles(path, imageList, config) {
-    var contents = FileSystemImageSlideshow.readdirSync(path);
-    for (let i = 0; i < contents.length; i++) {
-      var currentItem = path + '/' + contents[i];
-      var stats = FileSystemImageSlideshow.lstatSync(currentItem);
-      if (stats.isDirectory() && config.recursiveSubDirectories) {
-        this.getFiles(currentItem, imageList, config);
-      } else if (stats.isFile()) {
-        var isValidImageFileExtension = this.checkValidImageFileExtension(
-          currentItem,
-          config.validImageFileExtensions
-        );
-        if (isValidImageFileExtension) imageList.push(currentItem);
-      }
-    }
-  },
   // subclass socketNotificationReceived, received notification from module
   socketNotificationReceived: function(notification, payload) {
     if (notification === 'BACKGROUNDSLIDESHOW_REGISTER_CONFIG') {
       // this to self
       var self = this;
       // get the image list
-      var imageList = this.gatherImageList(payload);
-      // build the return payload
-      var returnPayload = {
-        identifier: payload.identifier,
-        imageList: imageList
-      };
-      // send the image list back
-      self.sendSocketNotification(
-        'BACKGROUNDSLIDESHOW_FILELIST',
-        returnPayload
-      );
+      this.gatherImageList(payload, function(err, imageList) {
+        if (err) {
+          return;
+        }
+        // build the return payload
+        var returnPayload = {
+          identifier: payload.identifier,
+          imageList: imageList
+        };
+        // send the image list back
+        self.sendSocketNotification(
+          'BACKGROUNDSLIDESHOW_FILELIST',
+          returnPayload
+        );
+      });
     }
   }
 });
